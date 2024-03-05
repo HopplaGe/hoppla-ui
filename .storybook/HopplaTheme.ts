142a9315e52f27def9a29b6f55b6dc3e512a6596
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-05 23:44:37 +0100

    Add HopplaTheme and update Storybook configuration

---
+++ .storybook/HopplaTheme.ts
@@ -0,0 +1,18 @@
+import {create} from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Hoppla UI Kit',
+    brandUrl: 'https://hoppla.ge/',
+    brandImage: 'https://www.hoppla.ge/_next/static/media/logo.eae669a3.svg',
+    brandTarget: '_self',
+    colorPrimary: '#e64e3d',
+    colorSecondary: '#e64e3d',
+
+    // UI
+    appBg: 'white',
+    appContentBg: 'white',
+    appBorderColor: 'white',
+    appBorderRadius: 4,
+    barSelectedColor: '#e64e3d',
+});
