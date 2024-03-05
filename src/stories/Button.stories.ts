1dda661d979699f2c70e189c5decaf62eed9fb63
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-05 23:44:59 +0100

    Refactor Button.stories.ts

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,68 +1,71 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type {Meta, StoryObj} from "@storybook/react";
 
-import { Button } from "../components";
+import {Button} from "../components";
 
 const meta = {
-  title: "Elements/Button",
-  component: Button,
-  parameters: {
-    layout: "centered",
-  },
-  tags: ["autodocs"],
+    title: "Elements/Button",
+    component: Button,
+    parameters: {
+        layout: "centered",
+        themes: {
+            themeOverride: 'light', // component level override
+        },
+    },
+    tags: ["autodocs"],
 
-  argTypes: {
-    startContent: { control: "text" },
-    endContent: { control: "text" },
-  },
+    argTypes: {
+        startContent: {control: "text"},
+        endContent: {control: "text"},
+    },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Solid: Story = {
-  args: {
-    label: "ღილაკი",
-    size: "sm",
-    type: "solid",
-    color: "primary",
-    animation: true,
-  },
+export const SolidPrimary: Story = {
+    args: {
+        label: "ღილაკი",
+        size: "sm",
+        type: "solid",
+        color: "primary",
+        animation: true,
+    },
 };
 
 export const Outline: Story = {
-  args: {
-    label: "ღილაკი",
-    size: "sm",
-    type: "outline",
-    color: "primary",
-    animation: true,
-    endContent: "🚀",
-  },
-  parameters: {
-    backgrounds: {
-      default: "red",
+    args: {
+        label: "ღილაკი",
+        size: "sm",
+        type: "outline",
+        color: "primary",
+        animation: true,
+        endContent: "🚀",
     },
-  },
+    parameters: {
+        backgrounds: {
+            default: "red",
+        },
+    },
 };
 
 export const Ghost: Story = {
-  args: {
-    label: "ღილაკი",
-    size: "sm",
-    type: "ghost",
-    color: "primary",
-    animation: true,
-  },
+    args: {
+        label: "ღილაკი",
+        size: "sm",
+        type: "ghost",
+        color: "primary",
+        animation: true,
+    },
 };
 
 export const Link: Story = {
-  args: {
-    label: "ღილაკი",
-    size: "sm",
-    type: "link",
-    color: "primary",
-    animation: true,
-    startContent: "🚀",
-  },
+    args: {
+        label: "ღილაკი",
+        size: "sm",
+        type: "link",
+        color: "primary",
+        animation: true,
+        startContent: "🚀",
+    },
 };
