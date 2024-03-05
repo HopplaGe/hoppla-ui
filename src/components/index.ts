801b9dac9e76ab593641913e4977c8ad20301f3f
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-05 20:43:55 +0100

    Add Logo component and update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-export { default as Button } from "./Button";
-export { default as Avatar } from "./Avatar";
-export { default as Input } from "./Input";
-export { default as DirectionsCard } from "./DirectionsCard";
-export { default as ListMenu } from "./ListMenu";
+export {default as Button} from "./Button";
+export {default as Avatar} from "./Avatar";
+export {default as Input} from "./Input";
+export {default as DirectionsCard} from "./DirectionsCard";
+export {default as ListMenu} from "./ListMenu";
+export {default as Logo} from "./Logo";
---
+++ src/stories/Logo.stories.tsx
@@ -0,0 +1,64 @@
+import type {Meta, StoryObj} from "@storybook/react";
+
+import {Logo} from "../components";
+
+const meta = {
+    title: "Elements/Logo",
+    component: Logo,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+    argTypes: {
+        variant: {
+            options: ["light", "dark"],
+            control: {
+                type: "select",
+            },
+        },
+        color: {
+            options: [
+                "primary",
+                "secondary",
+                "light",
+                "dark",
+                "info",
+                "warning",
+                "danger",
+                "success",
+                "white",
+                "black",
+            ],
+            control: {
+                type: "select",
+            },
+        },
+        size: {
+            options: ["sm", "lg"],
+            control: {
+                type: "select",
+            },
+        },
+    }
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Large: Story = {
+    args: {
+        variant: "dark",
+        color: "primary",
+        size: "lg",
+        onClick: () => console.log("clicked"),
+    },
+} satisfies Story;
+
+export const Small: Story = {
+    args: {
+        color: "primary",
+        size: "sm",
+        onClick: () => console.log("clicked"),
+    },
+} satisfies Story;
